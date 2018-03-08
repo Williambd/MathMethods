c0e4807e205513c75c100f51ddd059f403c83822
widea9928 <widea9928@7HGM6Y1.Student.UGDSB.ED>
2018-03-08 13:01:15 -0500

    minor edits

--- src/mathmethods/MathMethods.java
+++ src/mathmethods/MathMethods.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Made by WilliamBD
+ * On 08/03/2018
+ * To demonstrate various java.lang.Math methods
  */
 
 package mathmethods;
